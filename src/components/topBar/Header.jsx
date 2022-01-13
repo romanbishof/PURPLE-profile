382f24f9d76d9001ba48c9b9cb82062a6a799867
romanbishof <romanbishof@gmail.com>
2022-01-13 19:27:32 +0200

    second commit

--- src/components/topBar/Header.jsx
+++ src/components/topBar/Header.jsx
@@ -1,7 +1,5 @@
-import { AppProvider, Frame, TopBar } from '@shopify/polaris';
+import { AppProvider, TopBar } from '@shopify/polaris';
 import React, { useCallback, useState } from 'react';
-import logo from '../../assets/logo.svg'
-import profileImg from '../../assets/profileImg.png'
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
